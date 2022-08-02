9753da5ca145c818addc4b93c74b1e567eabdb54
Eugene Park <hellothxsorry@gmail.com>
2022-08-02 18:05:02 +0500

    Practical Task 1 completed

--- PracticalTasks/Program.cs
+++ PracticalTasks/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
+        static int maxUnequalChar(string str)
+        {
+            int strLength = str.Length;
+            int result = 0;
+
+            for (int i = 0; i < strLength; i++)
+            {
+                bool[] checkedChar = new bool[256];
+
+                for (int j = i; j < strLength; j++)
+                {
+                    if (checkedChar[str[j]] == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        result = Math.Max(result, j - i + 1);
+                        checkedChar[str[j]] = true;
+                    }
+                }
+                checkedChar[str[i]] = false;
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please type a sequence of symbols...");
+
+            var getSequence = Convert.ToString(Console.ReadLine());
+
+            int maxNum = maxUnequalChar(getSequence);
+
+            Console.WriteLine("The maximum number of unequal consecutive characters is: " + maxNum);
         }
     }
 }
