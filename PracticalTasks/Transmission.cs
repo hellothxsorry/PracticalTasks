a1925543159a47bc2d0d507d9ab10cdfe2fa1ffa
Eugene Park <hellothxsorry@gmail.com>
2022-10-01 03:13:40 +0500

    Task 4 is done

---
+++ PracticalTasks/Transmission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasks
+{
+    internal class Transmission
+    {
+        public string? Type;
+        public int? NumberOfGears;
+        public string? Manufacturer;
+
+        public Transmission(string? type, int? numberOfGears, string? manufacturer)
+        {
+            Type = type;
+            NumberOfGears = numberOfGears;
+            Manufacturer = manufacturer;
+        }
+    }
+}
---
+++ PracticalTasks/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasks
+{
+    class Vehicle
+    {
+        public string VehicleName;
+
+        public Vehicle(string name, Engine engine, Chassis chassis, Transmission transmission)
+        {
+            VehicleName = name;
+            Console.WriteLine("Type of vehicle: " + name + "\n" + "Engine power: " + engine.Power + " horsepower" + " | " +
+                "Engine volume: " + engine.Volume + "L" + " | " + "Engine type: " + engine.Type + " | " +
+                "Engine serial number: " + engine.SerialNumber + "\n" + "Number of wheels: " + chassis.WheelsNumber +
+                " | " + "The VIN (Vehicle Identification Number): " + chassis.VINumber + " | " +
+                "Loading capacity: " + chassis.PermissibleLoad + " kg" + "\n" + "Transmission type: " + transmission.Type +
+                " | " + "Number of gears: " + transmission.NumberOfGears + " | " + "Transmission manufacturer: " +
+                transmission.Manufacturer + "\n");
+        }
+    }
+}
