c0fc89a8a96c643214c173a8e531459c8529dab0
hellothxsorry <50228202+hellothxsorry@users.noreply.github.com>
2022-10-03 16:27:51 +0500

    Update Vehicle.cs

--- PracticalTasks/Vehicle.cs
+++ PracticalTasks/Vehicle.cs
@@ -13,13 +13,12 @@ namespace PracticalTasks
         public Vehicle(string name, Engine engine, Chassis chassis, Transmission transmission)
         {
             VehicleName = name;
-            Console.WriteLine("Type of vehicle: " + name + "\n" + "Engine power: " + engine.Power + " horsepower" + " | " +
-                "Engine volume: " + engine.Volume + "L" + " | " + "Engine type: " + engine.Type + " | " +
-                "Engine serial number: " + engine.SerialNumber + "\n" + "Number of wheels: " + chassis.WheelsNumber +
-                " | " + "The VIN (Vehicle Identification Number): " + chassis.VINumber + " | " +
-                "Loading capacity: " + chassis.PermissibleLoad + " kg" + "\n" + "Transmission type: " + transmission.Type +
-                " | " + "Number of gears: " + transmission.NumberOfGears + " | " + "Transmission manufacturer: " +
-                transmission.Manufacturer + "\n");
+            Console.WriteLine($"Type of vehicle: {name} \n Engine power: {engine.Power} horsepower | " +
+                $"Engine volume: {engine.Volume}L | Engine type: {engine.Type} | Engine serial number: " +
+                $"{engine.SerialNumber} \n Number of wheels: {chassis.WheelsNumber} | " +
+                $"The VIN (Vehicle Identification Number): {chassis.VINumber} | Loading capacity: " +
+                $"{chassis.PermissibleLoad} kg \n Transmission type: {transmission.Type} | Number of gears: " +
+                $"{transmission.NumberOfGears} | Transmission manufacturer: {transmission.Manufacturer} \n");
         }
     }
 }
