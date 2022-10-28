758ea889e7d3a616abdb6f98594ccfd95f3d3fc4
hellothxsorry <50228202+hellothxsorry@users.noreply.github.com>
2022-10-28 09:33:17 +0500

    Task 5 is completed

---
+++ 5 - Interfaces and abstract classes/PracticalTasks/Drone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasks
+{
+    internal class Drone : IFlyable
+    {
+        public bool isAnyRestriction { get; set; }
+        public int speed { get; set; } = 200;
+        public float startPosX { get; set; }
+        public float startPosY { get; set; }
+        public float startPosZ { get; set; }
+
+        double distance;
+        double flightTime;
+                
+        //1 min hovering every 10 min
+        //Travel time = distance / speed
+        //Additional time = travel time / 10
+        //Time of flight = distance / speed + additional time 
+        public double FlyTo(float x, float y, float z)
+        {
+            double distance = Math.Pow((Math.Pow(x - startPosX, 2) + Math.Pow(y - startPosY, 2) +
+                Math.Pow(z - startPosZ, 2) * 1.0), 0.5);
+
+            //Limit the maximum distance: the drone is capable to cover up to 200 km
+            if (distance > 200)
+            {
+                Console.WriteLine("The distance between starting and destination positions is too large: " +
+                    "the drone is capable to cover only 200 km. Please try again with other destination coordinates.");
+                isAnyRestriction = true;
+            }
+            else { isAnyRestriction = false; }
+
+            return distance;
+        }
+
+        public double GetFlyTime(double distance)
+        {
+            double flightTimeInMinutes = distance / speed * 60;
+
+            int hoveringPeriods;
+
+            if (flightTimeInMinutes < 10) { hoveringPeriods = 0; }
+            else { hoveringPeriods = (int)flightTimeInMinutes / 10; }
+
+            flightTime += distance / speed + hoveringPeriods / 60;
+
+            return flightTime;
+        }
+    }
+}
---
+++ 5 - Interfaces and abstract classes/PracticalTasks/IFlyable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasks
+{
+    internal interface IFlyable
+    {
+        bool isAnyRestriction { get; set;  }
+        int speed { get; set; }
+        float startPosX { get; set; }
+        float startPosY { get; set; }
+        float startPosZ { get; set; }
+
+        double FlyTo(float x, float y, float z);
+
+        double GetFlyTime(double distance);
+    }
+}
