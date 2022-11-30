4cd136c7010b1237cdea954ba23cd37240f3b3d2
hellothxsorry <50228202+hellothxsorry@users.noreply.github.com>
2022-11-30 09:40:32 +0500

    Update Transmission.cs

--- PracticalTasks/Transmission.cs
+++ PracticalTasks/Transmission.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PracticalTasks
 {
-    internal class Transmission
+    public class Transmission
     {
         public string? Type;
         public int? NumberOfGears;
