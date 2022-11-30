061f88821634f54ae63a1f78db63d047a3661f1e
hellothxsorry <50228202+hellothxsorry@users.noreply.github.com>
2022-11-30 09:32:11 +0500

    Update Program.cs

--- PracticalTasks/Program.cs
+++ PracticalTasks/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace PracticalTasks
 {
-    internal class Program
+    public class Program
     {
         static int maxUnequalChar(string str)
         {
@@ -9,29 +9,20 @@
 
             for (int i = 0; i < strLength; i++)
             {
-                //Array where each index represents symbols in ASCII codes: e.g. checkedChar[97] represents 'a'.
-                //True means that symbol was already met in a substring when we check a sequence of chars for a duplicate.
                 bool[] checkedChar = new bool[256];
 
-                //checking char by char until it detects a match with one of the previous steps.
                 for (int j = i; j < strLength; j++)
                 {
                     if (checkedChar[str[j]] == true)
                     {              
-                        //Once the duplicated char is found, stop going further
-                        //restart checking the sequence with a shift of the starting point.
                         break;
                     }
                     else
                     {
-                        //Comparison of the current max number of unique subsequent chars with a number from a new checking round.
-                        //Take the largest one as a new record.
                         result = Math.Max(result, j - i + 1);    
-                        //Mark the checked char as present for this round and be ready to be triggered by a duplicate of this char.
                         checkedChar[str[j]] = true;
                     }
                 }
-                //Reset the array for the next round.
                 checkedChar[str[i]] = false;
             }
             return result;
