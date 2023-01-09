612786c8d088783d6bb719480fa8387e15da0212
hellothxsorry <50228202+hellothxsorry@users.noreply.github.com>
2023-01-09 21:43:37 +0500

    Adding task 8

---
+++ ObjectOrientedDesignPrinciples/PracticalTasks/Commands/CountBrandsCommand.cs
@@ -0,0 +1,36 @@
+﻿using PracticalTasks.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasks.Commands
+{
+    public class CountBrandsCommand : ICommand
+    {
+        private SingletonRepository carsRepository;
+
+        public CountBrandsCommand(SingletonRepository carsRepository)
+        {
+            this.carsRepository = carsRepository;
+        }
+
+        public void Execute()
+        {
+            var currentList = carsRepository.GetCarsListInfo();
+            var brandsListWithoutDuplicate = currentList.GroupBy(x => x.Brand).Select(y => y.First()).ToList();
+            var brandsNumber = brandsListWithoutDuplicate.Count();
+
+            Console.Clear();
+
+            for (int i = 0; i < brandsNumber; i++)
+            {
+                Console.WriteLine($"{i + 1}. {brandsListWithoutDuplicate[i].Brand}\n");
+            }
+            Console.WriteLine($"Total number of brands: {brandsNumber}\n" +
+                $"----- ----- ----- ----- ----- ----- ----- ----- ----- -----\n");
+            
+        }
+    }
+}
