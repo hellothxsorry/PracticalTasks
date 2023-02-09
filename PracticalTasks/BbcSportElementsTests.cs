e473545c4a667975b4f8c1ad3e0f095f818ce8f6
Eugene Park <hellothxsorry@gmail.com>
2023-02-09 14:58:45 +0500

    Refactor

--- PracticalTasks/BbcSportElementsTests.cs
+++ PracticalTasks/BbcSportElementsTests.cs
@@ -7,46 +7,48 @@ namespace PracticalTasks
     public class BbcSportElementsTests : IDisposable
     {
         private IWebDriver driver;
+        private const string BaseUrl = "https://www.bbc.com/sport";
 
-        [Fact]
-        public void TestSearchField()
+        public BbcSportElementsTests()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.bbc.com/sport");
+            driver.Navigate().GoToUrl(BaseUrl);
+        }
 
-            IWebElement searchField = driver.FindElement(
+        [Fact]
+        public void CanLocateSearchFieldTests()
+        {
+            IWebElement searchFieldInput = driver.FindElement(
                 By.CssSelector("div[aria-label='Search BBC'][role='search']" +
                 "[class='ssrcss-1j3alh1-GlobalNavigationItem e1gviwgp23']"));
 
-            Assert.NotNull(searchField);
+            Assert.NotNull(searchFieldInput);
+            Assert.True(searchFieldInput.Displayed);
         }
 
         [Fact]
-        public void TestAllSportsButton()
+        public void CanLocateAllSportsButtonTests()
         {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.bbc.com/sport");
-
             IWebElement allSportsButton = driver.FindElement(By.LinkText("More"));
 
             Assert.NotNull(allSportsButton);
+            Assert.True(allSportsButton.Displayed);
         }
 
         [Fact]
-        public void TestBbcLogo()
+        public void CanLocateBbcLogoTests()
         {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.bbc.com/sport");
-
             IWebElement bbcIcon = driver.FindElement(
                 By.XPath("/html/body/div[2]/div/div/div[1]/div/header/nav/div[1]/div/div[1]/a"));
 
             Assert.NotNull(bbcIcon);
+            Assert.True(bbcIcon.Displayed);
         }
 
         public void Dispose()
         {
             driver.Quit();
+            driver.Dispose();
         }
     }
 }
--- PracticalTasks/XpathAxesTests.cs
+++ PracticalTasks/XpathAxesTests.cs
@@ -7,50 +7,52 @@ namespace PracticalTasks
     public class XpathAxesTests : IDisposable
     {
         private IWebDriver driver;
+        private const string BaseUrl = "https://www.bbc.com/sport";
 
-        [Fact]
-        public void TestSearchField()
+        public XpathAxesTests()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.bbc.com/sport");
-
-            IWebElement searchField = driver.FindElement(
-                By.XPath("//div[@role='search' and @aria-label='Search BBC']/" +
-                "*[@href='https://www.bbc.co.uk/search?d=SPORT_GNL']"));
-
-            Assert.NotNull(searchField);
+            driver.Navigate().GoToUrl(BaseUrl);
         }
 
         [Fact]
-        public void TestAllSportsButton()
+        public void CanLocateSearchFieldByXpathAxesTests()
         {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.bbc.com/sport");
+            IWebElement searchFieldInput = driver.FindElement(
+                By.XPath("//div[@role='search' and @aria-label='Search BBC']/" +
+                "*[@href='https://www.bbc.co.uk/search?d=SPORT_GNL']"));
 
+            Assert.NotNull(searchFieldInput);
+            Assert.True(searchFieldInput.Displayed);
+        }
+
+        [Fact]
+        public void CanLocateAllSportsButtonByXpathAxesTests()
+        {
             IWebElement allSportsButton = driver.FindElement(
                 By.XPath("//a[@href='/sport/all-sports' and " +
                 "@class='ssrcss-1kl7qyq-StyledToggle ekfn8591']/parent::div/preceding-sibling::div"));
 
             Assert.NotNull(allSportsButton);
+            Assert.True(allSportsButton.Displayed);
         }
 
         [Fact]
-        public void TestBbcLogo()
+        public void CanLocateBbcLogoByXpathAxesTests()
         {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.bbc.com/sport");
-
             IWebElement bbcLogo = driver.FindElement(
                 By.XPath("//a[@href='https://www.bbc.com' and @class=" +
                 "'ssrcss-dkv2k4-NavigationLink-LogoLink e1gviwgp14']/ancestor::nav" +
                 "[@aria-label='BBC' and @class='ssrcss-1a6al6l-ChameleonGlobalNavigation e140xmwz0']"));
 
             Assert.NotNull(bbcLogo);
+            Assert.True(bbcLogo.Displayed);
         }
 
         public void Dispose()
         {
             driver.Quit();
+            driver.Dispose();
         }
     }
 }
