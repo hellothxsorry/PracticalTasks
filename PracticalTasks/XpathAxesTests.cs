f07c78f2ce8e5341778b6321a385aea4772f9212
Eugene Park <hellothxsorry@gmail.com>
2023-02-10 16:25:54 +0500

    ref

--- PracticalTasks/XpathAxesTests.cs
+++ PracticalTasks/XpathAxesTests.cs
@@ -18,9 +18,7 @@ namespace PracticalTasks
         [Fact]
         public void CanLocateSearchFieldByXpathAxesTest()
         {
-            IWebElement searchFieldInput = driver.FindElement(
-                By.XPath("//div[@role='search' and @aria-label='Search BBC']/" +
-                "*[@href='https://www.bbc.co.uk/search?d=SPORT_GNL']"));
+            IWebElement searchFieldInput = driver.FindElement(By.XPath("//a[contains(@href,'search')]/child::span[text()='Search BBC']"));
 
             Assert.NotNull(searchFieldInput);
             Assert.True(searchFieldInput.Displayed);
@@ -29,9 +27,7 @@ namespace PracticalTasks
         [Fact]
         public void CanLocateAllSportsButtonByXpathAxesTest()
         {
-            IWebElement allSportsButton = driver.FindElement(
-                By.XPath("//a[@href='/sport/all-sports' and " +
-                "@class='ssrcss-1kl7qyq-StyledToggle ekfn8591']/parent::div/preceding-sibling::div"));
+            IWebElement allSportsButton = driver.FindElement(By.XPath("//div[@id='product-navigation-menu']/descendant::a[text()='More']"));
 
             Assert.NotNull(allSportsButton);
             Assert.True(allSportsButton.Displayed);
@@ -40,10 +36,7 @@ namespace PracticalTasks
         [Fact]
         public void CanLocateBbcLogoByXpathAxesTest()
         {
-            IWebElement bbcLogo = driver.FindElement(
-                By.XPath("//a[@href='https://www.bbc.com' and @class=" +
-                "'ssrcss-dkv2k4-NavigationLink-LogoLink e1gviwgp14']/ancestor::nav" +
-                "[@aria-label='BBC' and @class='ssrcss-1a6al6l-ChameleonGlobalNavigation e140xmwz0']"));
+            IWebElement bbcLogo = driver.FindElement(By.XPath("//span[contains(text(),'Homepage')]/parent::span"));
 
             Assert.NotNull(bbcLogo);
             Assert.True(bbcLogo.Displayed);
