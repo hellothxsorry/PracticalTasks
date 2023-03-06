c7bfee7779ecad0bd99d1500ffb05cc57e2578e8
hellothxsorry <50228202+hellothxsorry@users.noreply.github.com>
2023-03-06 14:39:08 +0500

    Framework refactored

---
+++ Framework/PracticalTasks/Models/ServerInstance.cs
@@ -0,0 +1,30 @@
+﻿namespace PracticalTasks.Model
+{
+    public class ServerInstance
+    {
+        public string NumberOfInstances { get; set; }
+        public string OperatingSystem { get; set; }
+        public string InstanceSeries { get; set; }
+        public string InstanceType { get; set; }
+        public string NumberOfGpu { get; set; }
+        public string GpuType { get; set; }
+        public string LocalSsd { get; set; }
+        public string DatacenterLocation { get; set; }
+        public string CommittedUsage { get; set; }
+
+        public ServerInstance(string numberOfInstances, string operatingSystem, 
+            string instanceSeries, string instanceType, string numberOfGpu, string gpuType,
+            string localSsd, string datacenterLocation, string committedUsage)
+        {
+            NumberOfInstances = numberOfInstances;
+            OperatingSystem = operatingSystem;
+            InstanceSeries = instanceSeries;
+            InstanceType = instanceType;
+            NumberOfGpu = numberOfGpu;
+            GpuType = gpuType;
+            LocalSsd = localSsd;
+            DatacenterLocation = datacenterLocation;
+            CommittedUsage = committedUsage;
+        }      
+    }
+}
---
+++ Framework/PracticalTasks/Pages/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using PracticalTasks.Utils;
+
+namespace PracticalTasks.Pages
+{
+    public class MainPage: AbstractPage
+    {
+        public MainPage(IWebDriver driver, WebDriverWait wait) : base(driver, wait) { }
+
+        public readonly string PageUrl = "https://cloud.google.com/";
+
+        private static By SearchInputLocator = By.Name("q");
+        
+        public IWebElement SearchInput => driver.FindElement(SearchInputLocator);         
+
+        public SearchResultsPage StartSearchFor(string request)
+        {
+            WaitingUtil.WaitUntilVisible(wait, SearchInputLocator);
+            SearchInput.SendKeys(request);
+            SearchInput.SendKeys(Keys.Enter);
+            return new SearchResultsPage(driver, wait);
+        }
+    }
+}
